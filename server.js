d1d15910943c47b9c8ddf51b1f5364dae9fe7b2e
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-16 14:28:40 -0600

    Import the auth, playlists, & tracks routers into server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 app.use(require("morgan")("dev")); // Can use instead of the logging middleware below
 app.use(express.json());
 
+app.use(require("./api/auth").router);
+app.use("/playlists", require("./api/playlists"));
+app.use("/tracks", require("./api/tracks"));
+
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
