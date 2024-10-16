b58228b1a90e2f14d4a9ec048a880470ad941c3a
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-16 14:14:23 -0600

    Create server.js, import express, express's morgan, add logging, 404, and error-handling middleware

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(require("morgan")("dev")); // Can use instead of the logging middleware below
+app.use(express.json());
+
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+// 404
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
+});
+
+// Error-handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong :(");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port#: ${PORT}...`);
+});
