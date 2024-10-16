ca93cb3c105327eab0d305f61d20e1a75813488c
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-16 14:44:53 -0600

    Extend the prisma client with custom methods register and login, with a hashed password

--- api/auth.js
+++ api/auth.js
@@ -3,3 +3,7 @@ const router = express.Router();
 
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
+
+function createToken(id) {
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: "3h" });
+}
--- prisma/index.js
+++ prisma/index.js
@@ -1,3 +1,29 @@
+const bcrypt = require("bcrypt");
+
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// Creates new instance of Prisma client and extends it with custom methods
+// for the user model
+const prisma = new PrismaClient().$extends({
+  // Extends the user model with custom methods for registration and login
+  model: {
+    user: {
+      async register(username, password, saltRounds = 10) {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const customer = await prisma.customer.create({
+          data: { email: username, password: hashedPassword },
+        });
+        return customer;
+      },
+      async login(username, password) {
+        const customer = await prisma.customer.findUniqueOrThrow({
+          where: { username },
+        });
+        const valid = await bcrypt.compare(password, customer.password);
+        if (!valid) throw Error("Invalid password");
+        return customer;
+      },
+    },
+  },
+});
+
 module.exports = prisma;
