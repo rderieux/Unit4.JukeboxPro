c5dce281f75b61ed600aa4429f5c736b3402aeb2
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-16 15:38:05 -0600

    Add get all tracks endpoint, import authenticate to tracks router

--- api/tracks.js
+++ api/tracks.js
@@ -3,3 +3,13 @@ const router = express.Router();
 module.exports = router;
 
 const prisma = require("../prisma");
+const { authenticate } = require("./auth");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tracks = await prisma.track.findMany();
+    res.json(tracks);
+  } catch (error) {
+    next(error);
+  }
+});
