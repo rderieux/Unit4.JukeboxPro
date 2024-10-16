ec554618c3cc7e1d574e25cb30ca1a66a289ef51
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-16 15:57:53 -0600

    Add tracks router to get by id, include playlists with track if logged in

--- api/tracks.js
+++ api/tracks.js
@@ -3,12 +3,41 @@ const router = express.Router();
 module.exports = router;
 
 const prisma = require("../prisma");
-const { authenticate } = require("./auth");
 
 router.get("/", async (req, res, next) => {
   try {
     const tracks = await prisma.track.findMany();
     res.json(tracks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const track = await prisma.track.findUniqueOrThrow({
+      where: { id: +id },
+    });
+
+    let response = { track };
+
+    if (req.user) {
+      const userPlaylists = await prisma.playlist.findMany({
+        where: {
+          ownerId: req.user.id,
+          tracks: {
+            some: {
+              id: +id,
+            },
+          },
+        },
+      });
+
+      reponse.userPlaylists = userPlaylists;
+    }
+    res.json(response);
   } catch (error) {
     next(error);
   }
