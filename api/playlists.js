382390f8e9c9a1ee4535964380569378893d61a6
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-17 10:00:18 -0600

    Add a post playlist route, and a get playlist by id route

--- api/playlists.js
+++ api/playlists.js
@@ -2,16 +2,52 @@ const express = require("express");
 const router = express.Router();
 module.exports = router;
 
-const prisma = require("../prisma");
 const { authenticate } = require("./auth");
+const prisma = require("../prisma");
 
 router.get("/", authenticate, async (req, res, next) => {
   try {
     const playlists = await prisma.playlist.findMany({
-      where: { userId: req.user.id },
-      include: { tracks: true },
+      where: { ownerId: req.user.id },
     });
     res.json(playlists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", authenticate, async (req, res, next) => {
+  const { name, description, trackIds } = req.body;
+
+  try {
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        ownerId: req.user.id,
+        tracks: { connect: trackIds.map((id) => ({ id })) },
+      },
+    });
+    res.json(playlist);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const playlist = await prisma.playlist.findUniqueOrThrow({
+      where: { id: +id },
+      include: { tracks: true },
+    });
+    if (playlist.ownerId !== req.user.id) {
+      next({
+        status: 403,
+        message: `Your userid: ${req.user.id} is not the owner of the requested playlist.`,
+      });
+    }
+    res.json(playlist);
   } catch (error) {
     next(error);
   }
